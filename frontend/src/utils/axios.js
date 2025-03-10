ddbcfd2bdd62a87a9a51fb22dfe3ad393b37ebf2
ujjwalghising <ujjwalghising567@gmail.com>
2025-03-10 11:07:10 +0530

    changed axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,9 +1,7 @@
-// src/utils/axios.js
 import axios from "axios";
 
-// Access backend URL using import.meta.env
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://sensible-production-4915.up.railway.app",  // Use VITE_ prefix
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://sensible-production-4915.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
